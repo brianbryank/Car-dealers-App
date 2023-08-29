873979ee7ed98700f19fb71d8c7c5027b4e4b3d0
brianbryank <brianbryan0125@gmail.com>
2023-08-29 22:42:23 +0300

    update file

---
+++ dealers-app/src/components/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Cart({ cartItems, removeFromCart }) {
+  return (
+    <div className="cart">
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <ul>
+          {cartItems.map(item => (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <span>${item.price}</span>
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Cart;

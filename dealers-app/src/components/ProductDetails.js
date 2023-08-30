0b503f71b15d8bd99818ca126049e9a16b97f028
+ <+>
2023-08-30 16:12:06 +0300

    added files

--- dealers-app/src/components/ProductDetails.js
+++ dealers-app/src/components/ProductDetails.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const ProductDetails = ({ product }) => {
+  return (
+    <div>
+      <h2>Product Details</h2>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
+};
+
+export default ProductDetails;

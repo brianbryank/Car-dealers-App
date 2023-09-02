5ada6d0ae651826ec036fe68ac4338cbf2538d9b
robertnjunge <mubearobert@gmail.com>
2023-09-02 13:40:04 +0300

    added changes

--- dealers-app/src/components/Cart.js
+++ dealers-app/src/components/Cart.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Cart = ({ inCart, onRemove }) => {
   const [totalCost, setTotalCost] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     calculateTotalCost();
@@ -17,11 +17,15 @@ const Cart = ({ inCart, onRemove }) => {
     setTotalCost(totalPrice);
   };
 
+  const handleCheckOut = () => {
+    navigate("/checkout", { state: { totalCost: totalCost } });
+  };
+
   return (
     <div className="product-container">
       <div className="checkout-link">
         <p className="total-cost">Total Cost: ${totalCost}</p>
-        <Link to="/checkout">Checkout</Link>
+        <button onClick={handleCheckOut}>Check Out</button>
       </div>
       {inCart.map((product, index) => (
         <div className="product" key={index}>
@@ -32,9 +36,7 @@ const Cart = ({ inCart, onRemove }) => {
           />
           <p className="product-name">{product.product_name}</p>
           <p className="product-price">${product.unit_price}</p>
-          <p>{product.product_description}</p>
           <p>rank: {product.ranking}</p>
-          <p>{product.created}</p>
           <button onClick={() => onRemove(product)}>Remove</button>
         </div>
       ))}
--- dealers-app/src/components/CheckoutForm.js
+++ dealers-app/src/components/CheckoutForm.js
@@ -1,53 +1,76 @@
-import React, { useState } from 'react';
-import '../App.css';
+// import React, { useState } from 'react';
 
-const CheckoutForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    address: '',
-    cardNumber: '',
-    expiryDate: '',
-    cvv: '',
-    cost: 0, // Add a default value
-  });
+// const CheckoutForm = ({totalCost,onRemove}) => {
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     address: '',
+//     cardNumber: '',
+//     expiryDate: '',
+//     cvv: '',
+//   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted:', formData);
-    // You can add further logic here, like sending the data to a server
-  };
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   };
 
-  return (
-    <div className="checkout-form-container">
-      <h2>Checkout</h2>
-      <form onSubmit={handleSubmit}>
-        <label className="checkout-form-label">
-          Name:
-          <br />
-          <input
-            placeholder='Write your full name'
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        {/* Rest of the form elements */}
-        <br />
-        <button className="checkout-form-button" type="submit">Place Order</button>
-      </form>
-    </div>
-  );
-};
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log('Form submitted:', formData);
 
-export default CheckoutForm;
+//   };
+
+//   return (
+//     <div className="checkout-form-container">
+//       <h2>Checkout</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label className="checkout-form-label"><br />
+//           Name:
+//           <br />
+//           <input placeholder='Full Name' type="text" name="name" value={formData.name} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   Email:
+//   <br />
+//   <input placeholder='E-mail Address' type="email" name="email" value={formData.email} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   Address:
+//   <br />
+//   <textarea placeholder='Address' name="address" value={formData.address} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   Card Number:
+//   <br />
+//   <input  type="text" name="cardNumber" value={formData.cardNumber} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   Expiry Date:
+//   <br />
+//   <input type="text" name="expiryDate" value={formData.expiryDate} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   CVV:
+//   <br />
+//   <input type="text" name="cvv" value={formData.cvv} onChange={handleChange} required />
+// </label>
+// <label className="checkout-form-label">
+//   Total Cost (USD): 
+//   <br />
+// <input type="number" name="cost" value= {totalCost} onChange={handleChange} disabled  />
+// </label>
+// <br />
+// <button onClick={onRemove} className="checkout-form-button" type="submit">Place Order</button>
+
+// </form>
+// </div>
+
+// );
+// };
+
+// export default CheckoutForm;
